e90621a41d81d9198605b1514bfbc53f01010b4d
jsjqian <jack.qian97@gmail.com>
2016-01-17 03:58:12 -0500

    Made da app werk

--- app/src/main/java/com/example/jack/fitrhythmapp/MainActivity.java
+++ app/src/main/java/com/example/jack/fitrhythmapp/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,PickSongsActivity.class);
+                Intent intent = new Intent(MainActivity.this,RunningActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/jack/fitrhythmapp/RunningActivity.java
+++ app/src/main/java/com/example/jack/fitrhythmapp/RunningActivity.java
@@ -1,8 +1,11 @@
 package com.example.jack.fitrhythmapp;
 
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -15,16 +18,32 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import java.lang.Thread;
+
+import org.w3c.dom.Text;
+
+import java.lang.Math;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class RunningActivity extends AppCompatActivity implements SensorEventListener {
 
     private float mLastX, mLastY, mLastZ;
+    private double max_Volume = 1300;
+    private double magnitude_total;
+    private double current_mag, previous_mag;
     private boolean mInitialized;
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
+    private ArrayList<Double> magnitudes = new ArrayList<Double>();
     private final float NOISE = (float) 2.0;
+    private double threshold = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +53,58 @@ public class RunningActivity extends AppCompatActivity implements SensorEventLis
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        final Button startstop = (Button)findViewById(R.id.start_stop_button);
+        startstop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                switch (startstop.getText().toString()) {
+                    case ("start"): {
+                        startstop.setText("stop");
+                        int volume = (int) (max_Volume * .6);
+                        audio.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+                        previous_mag = magnitude_total;
+                        break;
+                    }
+                    case ("stop"): {
+                        startstop.setText("start");
+                        audio.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+                        break;
+                    }
+                }
+            }
+        });
+
+
+
+        SeekBar seek = (SeekBar)findViewById(R.id.seek_bar);
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                double percent = Double.valueOf(progress);
+                TextView vol = (TextView) findViewById(R.id.max_volume);
+                max_Volume = percent * 0.01 * maxVolume;
+                vol.setText(Double.toString(percent));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+
     }
+
 
     protected void onResume() {
         super.onResume();
@@ -78,7 +148,52 @@ public class RunningActivity extends AppCompatActivity implements SensorEventLis
             tvX.setText(Float.toString(deltaX));
             tvY.setText(Float.toString(deltaY));
             tvZ.setText(Float.toString(deltaZ));
-            Log.i("MyActivity", Float.toString(deltaX) + "," + Float.toString(deltaY) + "," + Float.toString(deltaZ));
+            double magnitude = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2) + Math.pow(deltaZ, 2));
+            magnitudes.add(magnitude);
+            if (magnitudes.size() == 10) {
+                double total = 0;
+                for(int i = 0; i < 10; i++) {
+                        total += magnitudes.get(i);
+                }
+                magnitude_total = total/10;
+                magnitudes.clear();
+                TableLayout table = (TableLayout)findViewById(R.id.magnitude_table);
+                TableRow row = new TableRow(this);
+                TextView text = new TextView(this);
+                text.setText(Double.toString(total));
+                row.addView(text);
+                table.addView(row);
+                final Button startstop = (Button)findViewById(R.id.start_stop_button);
+                if(startstop.getText().toString() == "stop") {
+                    current_mag = magnitude_total;
+                    AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                    int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+                    if(current_mag - previous_mag > threshold) {
+                        if(currentVolume < max_Volume) {
+                            audio.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (currentVolume + .3*max_Volume), 0);
+                        }
+                        previous_mag = current_mag;
+                    }
+                    else if (current_mag - previous_mag < -1*threshold) {
+                        if (currentVolume > 0) {
+                            audio.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (currentVolume - .2*max_Volume), 0);
+                        }
+                        previous_mag = current_mag;
+                    }
+                    else {
+                    }
+                    if(current_mag == 0) {
+                        audio.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume/3, 0);
+                    }
+                }
+
+                AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+                int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                int volume = (int) (currentVolume *100 / maxVolume);
+                TextView vol = (TextView) findViewById(R.id.current_volume);
+                vol.setText(Double.toString(volume));
+            }
         }
     }
 }
